7e88d93fca435aac99afbcbb41f6b1991b8ce862
pujie <puji@gmail.com>
2018-06-28 11:13:22 +0700

    use config to get server and port setting ing help

--- js/help.js
+++ js/help.js
@@ -1,3 +1,7 @@
+var config = require("./js/configs.js"),
+    appconfig = config.appserver(),
+    server = appconfig.name,
+    port = appconfig.port;
 getdata = function(method){
     switch(method){
         case 'getvendors' :
@@ -22,7 +26,7 @@ getdata = function(method){
                 name:"savevendor",
                 format:"json",
                 description:"save a vendor into database",
-                syntax:'curl -d "name=vendors name&address=vendors address&phone=vendors phone&bankaccount=vendors bank account&createuser=vendors create user" -X POST http://localhost:2018/savevendor'
+                syntax:'curl -d "name=vendors name&address=vendors address&phone=vendors phone&bankaccount=vendors bank account&createuser=vendors create user" -X POST http://'+server+':'+port+'/savevendor'
             }
         case 'updatevendor' : 
             return {
@@ -30,8 +34,8 @@ getdata = function(method){
                 name:"updatevendor",
                 format:"json",
                 description:"update a vendor into database",
-                syntax:'curl -d "id=vendors id&name=vendors name&address=vendors address&phone=vendors phone&bankaccount=vendors bank account&createuser=vendors createuser" -X POST http://localhost:2018/savevendor',
-                example:'curl -d "id=1&name=ABC, PT&address=jl mayjen sungkono 83&phone=031 88776655&bankaccount=001234455667&createuser=puji" -X POST http://localhost:2018/updatevendor'
+                syntax:'curl -d "id=vendors id&name=vendors name&address=vendors address&phone=vendors phone&bankaccount=vendors bank account&createuser=vendors createuser" -X POST http://'+server+':'+port+'/savevendor',
+                example:'curl -d "id=1&name=ABC, PT&address=jl mayjen sungkono 83&phone=031 88776655&bankaccount=001234455667&createuser=puji" -X POST http://'+server+':'+port+'/updatevendor'
             }
         case 'getproducts' :
             return {
@@ -55,8 +59,8 @@ getdata = function(method){
                 name:"saveproduct",
                 format:"json",
                 description:"save a product into database",
-                syntax:'curl -d "name=laptop&partnumber=112 334 53436&unit=buah&price=4750000&discountlevel=1" -X POST http://<%= data.server %>:<%= data.port %>/saveproduct',
-                example:'curl -d "name=laptop&partnumber=112 334 53436&unit=buah&price=4750000&discountlevel=1" -X POST http://<%= data.server %>:<%= data.port %>/saveproduct'
+                syntax:'curl -d "name=laptop&partnumber=112 334 53436&unit=buah&price=4750000&discountlevel=1" -X POST http://'+server+':'+port+'/saveproduct',
+                example:'curl -d "name=laptop&partnumber=112 334 53436&unit=buah&price=4750000&discountlevel=1" -X POST http://'+server+':'+port+'/saveproduct'
             }
         case 'updateproduct' : 
             return {
@@ -64,8 +68,8 @@ getdata = function(method){
                 name:"updateproduct",
                 format:"json",
                 description:"update a product into database",
-                syntax:'curl -d "id=product id&name=product name&partnumber=product partnumber&unit=product unit&price=product price&discountlevel=product discountlevel" -X POST http://<%= data.server %>:<%= data.port %>/saveproduct',
-                example:'curl -d "id=1&name=laptop&partnumber=112 334 53436&unit=buah&price=4750000&discountlevel=1" -X POST http://<%= data.server %>:<%= data.port %>/updateproduct'
+                syntax:'curl -d "id=product id&name=product name&partnumber=product partnumber&unit=product unit&price=product price&discountlevel=product discountlevel" -X POST http://'+server+':'+port+'/saveproduct',
+                example:'curl -d "id=1&name=laptop&partnumber=112 334 53436&unit=buah&price=4750000&discountlevel=1" -X POST http://'+server+':'+port+'/updateproduct'
             }
         default:
             return {
