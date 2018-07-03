ec92289fe888f9eb084f56f002c5158e07767114
pujie <puji@gmail.com>
2018-07-03 16:22:45 +0700

    add help for saveuser

--- js/help.js
+++ js/help.js
@@ -95,7 +95,8 @@ getdata = function(method){
                 name:'saveuser',
                 format:'json',
                 description:'save a user',
-                syntax:''
+                syntax:'curl -d "username=username&password=user password&email=user email&level=user level&createuser=user who create this user" -X POST http://localhost:2018/saveuser',
+                example:'curl -d "username=agus&password=agus&email=agus@padi.net.id&level=1&createuser=puji" -X POST http://localhost:2018/saveuser'
             }
         case 'changepassword':
             return {
