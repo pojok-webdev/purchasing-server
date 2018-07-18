eaa4c20b78a3f771f53757f7bb3498e22b75b3fc
pujie <puji@gmail.com>
2018-07-18 10:16:13 +0700

    show result after successfull login

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/login',(req,res) => {
             var payload = {id:_result.id,name:_result.username,email:_result.email,defaultRoute:result.defaultRoute}
             var token = jwt.sign(payload,secretOrKey,{expiresIn:config.jwt().expiresIn})
             console.log('token',token)
-            console.log("RESULT",result)
+            console.log("RESULT",_result)
             _result.token = token
             _result.message = "ok"
             res.send(_result)
