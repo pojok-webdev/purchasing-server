d693091a1716cf22bcbbdaf7c87d9f7ebfad18e7
pujie <puji@gmail.com>
2018-08-09 12:54:51 +0700

    test header for image

--- index.js
+++ index.js
@@ -462,7 +462,7 @@ app.post('/getuser',(req,res)=>{
 app.get('/getuserimage/:id',(req,res)=>{
     con.getdata(query.getUserImage({id:req.params.id}),result=>{
         console.log("GetUserImageResult",result)
-        res.header("Content-Type", "jpeg");
+        //res.header("Content-Type", "jpeg");
 //        res.header("Content-Type", "application/octet-stream");
         res.end(result[0].image,'binary')
     })
